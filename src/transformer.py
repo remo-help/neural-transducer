02f7cb806fe1829ccb63bcabe8713a6961e0688b
remo-help <1163925remo@gmail.com>
2024-05-30 13:53:59 +0200

    fixing import issues

--- src/transformer.py
+++ src/transformer.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from src.dataloader import PAD_IDX
+from dataloader import PAD_IDX
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
