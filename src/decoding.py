d6e234c9798202d03dbd48f7a789bd8d2fa1010f
remo-help <1163925remo@gmail.com>
2024-05-30 13:05:37 +0200

    adds init

--- src/decoding.py
+++ src/decoding.py
@@ -3,9 +3,9 @@ from collections import namedtuple
 import torch
 
 import util
-from dataloader import BOS_IDX, EOS_IDX, STEP_IDX
-from model import HardMonoTransducer, HMMTransducer, dummy_mask
-from transformer import Transformer
+from .dataloader import BOS_IDX, EOS_IDX, STEP_IDX
+from .model import HardMonoTransducer, HMMTransducer, dummy_mask
+from .transformer import Transformer
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
