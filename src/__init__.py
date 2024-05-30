8a19faff03dc1c6353647ff761adefc36ae5121f
remo-help <1163925remo@gmail.com>
2024-05-30 12:59:42 +0200

    adds init

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,11 @@
-import loader
+from . import align
+from . import dataloader
+from . import util
+from . import decoding
+from . import inference
+from . import model
+from . import test
+from . import train
+from . import trainer
+from . import transformer
+from . import loader
