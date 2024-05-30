8c3338603ec41a94a99d171612e7896a352ad914
remo-help <1163925remo@gmail.com>
2024-05-30 13:10:48 +0200

    fixing impoty issues

--- src/inference.py
+++ src/inference.py
@@ -5,12 +5,9 @@ import numpy as np
 import pickle as pkl
 from . import loader
 
-args = {
-        'model':'/home/ubuntu/transducer-rework/neural-transducer/checkpoints/transformer/transformer/transformer-dene0.3/latin-high-.nll_0.8365.acc_90.2491.dist_0.0521.epoch_85',
-        'vocab': '/home/ubuntu/transducer-rework/neural-transducer/checkpoints/transformer/transformer/transformer-dene0.3/latin-high-.vocab',
-        'file': '/home/ubuntu/transducer-rework/neural-transducer/data/latin-dev',
-        'mode': 'testing'}
+if __name__ == "__main__":
+        args = {}
 
-loader = loader.Loader(cli=False, config=args)
+        loader = loader.Loader(cli=False, config=args)
 
-loader.run()
+        loader.run()
